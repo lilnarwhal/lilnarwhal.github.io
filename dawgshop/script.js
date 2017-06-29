7320404fbf8b3fa8481357f5e311520967f01307
Lily Samuel <lns.fashiongirl@gmail.com>
2017-06-29 18:43:47 +0000

    Dawg It Up

--- dawgshop/script.js
+++ dawgshop/script.js
@@ -1,5 +1,5 @@
 document.getElementById('go').onclick = function() {
 var inputText = $('#text-input').val ();
-var dawgifiedText = inputText + ', dawg';
+var dawgifiedText = inputText + ' is reading this and is wasting their time.';
 document.getElementById('output').innerHTML = dawgifiedText;
 }
